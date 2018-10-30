b255521746922a2ce8740ee110577dd8a8a238a7
Valentin Berruer <valb37@icloud.com>
2018-10-30 18:23:36 +0100

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'; 
+import { AccueilComponent } from './accueil/accueil.component'
+import {  } from
 
+const routes: Routes = [
+{ path: 'default', component: AccueilComponent },
+{ path: '', redirectTo: '/default', pathMatch: 'full' },
+
+]
 
 @NgModule({
-  imports: [],
+	exports: [ RouterModule ],
+	imports: [ RouterModule.forRoot(routes) ],
+
 })
 export class AppRoutingModule { }
