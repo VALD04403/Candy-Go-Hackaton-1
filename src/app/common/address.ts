6c31647a4fe3775d0a69247966a0e8830c90a6d9
bncordier <mrbcordier@gmail.com>
2018-10-30 17:01:46 +0100

    Geolocalisation visuelle de map 0.1

---
+++ src/app/common/address.ts
@@ -0,0 +1,10 @@
+
+const markers = [
+    {
+      "name": "Tours",
+      "lat": 47.383333,
+      "lng": 0.683333
+    }
+ ];
+
+ export default markers;
---
+++ src/app/common/addresse.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import marker from './address';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddresseService {
+  Addresses:any;
+
+  constructor() { }
+}
